fa11ce7c50fc7d8ae838d6951ceaebe8675defaf
Andrey <120014414+ArmyAlc@users.noreply.github.com>
2023-01-09 11:39:12 +0400

    simple

--- Budget Manager/task/src/budget/Main.java
+++ Budget Manager/task/src/budget/Main.java
@@ -1,9 +1,30 @@
 package budget;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+        double sum = 0;
+
+        ArrayList<String> purchases = new ArrayList<>();
+
+        while(scanner.hasNext()) {
+            purchases.add(scanner.nextLine());
+        }
+
+        for (String s : purchases) {
+            System.out.println(s);
+            String[] arr;
+            arr = s.split("\\$");
+            sum += Double.parseDouble(arr[1]);
+        }
 
 
+
+        System.out.println("Total: $" + sum);
     }
 }
